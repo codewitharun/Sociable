3009e654bba7775c1601dcda8d4ed479b894a1be
arun.kumar <arun.kumar@otssolutions.com>
2022-11-04 18:15:58 +0530

    4 nov work

--- src/screens/authscreens/Forgot.js
+++ src/screens/authscreens/Forgot.js
@@ -49,10 +49,6 @@ function Forgot({navigation}) {
           console.log('User reset link sent successfully');
         })
         .catch(error => {
-          if (error.code === 'auth/email-already-in-use') {
-            console.log('That email address is already in use!');
-          }
-
           if (error.code === 'auth/invalid-email') {
             console.log('That email address is invalid!');
           }
