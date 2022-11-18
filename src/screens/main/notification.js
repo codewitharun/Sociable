ee5ecf705daea8c54c5453f851c816eb2b74c38e
arun.kumar <arun.kumar@otssolutions.com>
2022-11-18 20:32:30 +0530

    18 nov changes

--- src/screens/main/notification.js
+++ src/screens/main/notification.js
@@ -5,14 +5,14 @@ import {TextInput} from 'react-native';
 import {Button} from 'react-native-vector-icons/MaterialCommunityIcons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {getASync} from '../../redux/action/asyncUpdateAction';
-import {firebase} from '@react-native-firebase/messaging';
+
 import {useSelector, useDispatch} from 'react-redux';
 import {getUser} from '../../redux/action/firebaseActions';
 import messaging from '@react-native-firebase/messaging';
 import {height, width} from '../components/Colors';
 const Notifications = ({navigation}) => {
   const {user} = useSelector(state => state.fromReducer);
-  // console.log('user in notifications screen', user);
+  console.log('user in notifications screen', user);
   const [refreshing, setRefreshing] = useState(false);
   // console.log('users from drawer screen', users.displayName);
   const dispatch = useDispatch();
@@ -22,40 +22,18 @@ const Notifications = ({navigation}) => {
       notified();
       setRefreshing(true);
       fetchUser();
-      // notify();
+
       setRefreshing(false);
     }, 2000);
   }, []);
 
   function notified() {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert(
-        'A new FCM message arrived!',
-        remoteMessage?.notification?.body,
-      );
-      console.log(JSON.stringify(JSON.stringify(remoteMessage)));
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
     });
 
     return unsubscribe;
-  }
-  notified();
-  // async function notify() {
-  //   (await Platform.OS) == 'android'
-  //     ? firebase.messaging().sendMessage({
-  //         tokens: [
-  //           'foyucm5ISeGBtbuVFnmkYy:APA91bGYCa4bvvWoS7_TVMH9yYnJbKFRdGIl7LH5QLuhM9JoAQVNkoZH3DncnkIGvuYoBCDzwuB7IpWsCVGvVwznSZ8e4_LKxrbRseHrtN66p-ZFDh6d6v61NPMRH4rJu9bFz3pxMvju',
-  //         ], // ['token_1', 'token_2', ...]
-  //         notification: {
-  //           title: 'Basic Notification',
-  //           body: 'This is a basic notification sent from the server!',
-  //           // imageUrl: 'https://my-cdn.com/app-logo.png',
-  //         },
-  //         data: {
-  //           title: 'hello user',
-  //         },
-  //       })
-  //     : null;
-  // }
+  }, []);
 
   return (
     <SafeAreaView style={{backgroundColor: 'black'}}>
