f0b1bef0b90a085f383c4924e7036e48369fea41
arun.kumar <arun.kumar@otssolutions.com>
2022-11-18 20:32:30 +0530

    18 nov changes

--- src/screens/main/notification.js
+++ src/screens/main/notification.js
@@ -5,14 +5,14 @@ import {TextInput} from 'react-native';
 import {Button} from 'react-native-vector-icons/MaterialCommunityIcons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {getASync} from '../../redux/action/asyncUpdateAction';
-
+import {firebase} from '@react-native-firebase/messaging';
 import {useSelector, useDispatch} from 'react-redux';
 import {getUser} from '../../redux/action/firebaseActions';
 import messaging from '@react-native-firebase/messaging';
 
 const Notifications = ({navigation}) => {
   const {user} = useSelector(state => state.fromReducer);
-  console.log('user in notifications screen', user);
+  // console.log('user in notifications screen', user);
   const [refreshing, setRefreshing] = useState(false);
   // console.log('users from drawer screen', users.displayName);
   const dispatch = useDispatch();
@@ -21,18 +21,36 @@ const Notifications = ({navigation}) => {
     setTimeout(() => {
       setRefreshing(true);
       fetchUser();
-
       setRefreshing(false);
     }, 2000);
   }, []);
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      Alert.alert(
+        'A new FCM message arrived!',
+        remoteMessage?.notification?.body,
+      );
+      console.log(JSON.stringify(JSON.stringify(remoteMessage)));
     });
 
     return unsubscribe;
   }, []);
-
+  // async function notify() {
+  //   await firebase.messaging().sendMessage({
+  //     tokens: [
+  //       'foyucm5ISeGBtbuVFnmkYy:APA91bGYCa4bvvWoS7_TVMH9yYnJbKFRdGIl7LH5QLuhM9JoAQVNkoZH3DncnkIGvuYoBCDzwuB7IpWsCVGvVwznSZ8e4_LKxrbRseHrtN66p-ZFDh6d6v61NPMRH4rJu9bFz3pxMvju',
+  //     ], // ['token_1', 'token_2', ...]
+  //     notification: {
+  //       title: 'Basic Notification',
+  //       body: 'This is a basic notification sent from the server!',
+  //       // imageUrl: 'https://my-cdn.com/app-logo.png',
+  //     },
+  //     data: {
+  //       title: 'hello user',
+  //     },
+  //   });
+  // }
+  // notify();
   return (
     <SafeAreaView>
       <View>
