d237fd45151c71fa9d99f1a150a5de23d79e94c3
codewitharun <arunk4it@gmail.com>
2023-02-13 23:16:23 +0530

    app center merge code

--- src/screens/main/notification.js
+++ src/screens/main/notification.js
@@ -1,77 +1,53 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
-import {GiftedChat} from 'react-native-gifted-chat';
-import messaging from '@react-native-firebase/messaging';
-import auth from '@react-native-firebase/auth';
+import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import {firebase} from '@react-native-firebase/database';
+// import 'firebase/database';
+
 const Notifications = () => {
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState('');
-  const [fcmToken, setfcmToken] = useState('');
-  const [ChatRoomId, setChatRoomId] = useState('');
-  const user1 = 'Arun';
-  const user2 = 'Dhananjay';
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
-    const chatRef = firebase.database().ref('/chat');
-    chatRef.on('value', snapshot => {
-      const newMessages = [];
-      snapshot.forEach(child => {
-        newMessages.push(child.val());
+    const unsubscribe = firebase
+      .database()
+      .ref('chatRooms')
+      .on('value', snapshot => {
+        // const messages = snapshot.val();
+        snapshot.forEach(element => {
+          // console.log(element);
+          setUsers(element);
+        });
+
+        console.log('uniqueUsers', users);
       });
-      setMessages(newMessages);
-    });
+
+    return () => unsubscribe();
   }, []);
 
-  useEffect(() => {
-    messaging()
-      .getToken()
-      .then(fcmToken => {
-        console.log(fcmToken);
-        setfcmToken(fcmToken);
-        // send the FCM token to your server
-      });
-    const chatRoomId = [user1, user2].sort().join('-');
-    setChatRoomId(chatRoomId);
-    const chatRef = firebase.database().ref(`chatRooms/${chatRoomId}`);
-    chatRef.on('value', snapshot => {
-      const message = snapshot.val();
-      const messages = [];
-      snapshot.forEach(message => {
-        messages.push(message.val());
-      });
-      setMessages(messages);
-      // sendNotification(fcmToken, message);
-    });
-
-    return () => {
-      chatRef.off();
-    };
-  }, [user1, user2]);
-  const onSend = newMessages => {
-    // newMessages.forEach(message => {
-    //   firebase.database().ref('/chat').push(message);
-    // });
-    const chatRoomId = [user1, user2].sort().join('-');
-
-    firebase.database().ref(`chatRooms/${chatRoomId}`).push({
-      sender: user1,
-      message: newMessage,
-      timestamp: firebase.database.ServerValue.TIMESTAMP,
-    });
-    setNewMessage('');
-  };
-
   return (
-    <View style={{flex: 1}}>
-      <GiftedChat
-        messages={newMessage}
-        onSend={onSend}
-        user={{
-          _id: user1,
-        }}
-      />
-    </View>
+    <FlatList
+      data={users}
+      renderItem={({item}) => (
+        <TouchableOpacity style={styles.userContainer}>
+          {console.log('item', item)}
+          <Text style={styles.userText}>{item}</Text>
+        </TouchableOpacity>
+      )}
+      keyExtractor={item => item}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  userContainer: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
+  },
+  userText: {
+    fontSize: 16,
+    color: 'black',
+  },
+});
 
 export default Notifications;
