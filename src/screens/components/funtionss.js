63969d4933b904e246dfc1b71f610c579d7bca87
codewitharun <arunk4it@gmail.com>
2023-04-27 00:36:39 +0530

    backend code init

--- src/screens/components/funtionss.js
+++
@@ -1,7 +0,0 @@
-import {PermissionsAndroid} from 'react-native';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-import auth from '@react-native-firebase/auth';
-import storage from '@react-native-firebase/storage';
-import firestore from '@react-native-firebase/firestore';
-import {useState, useffect} from 'react';
-
