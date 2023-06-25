e8a465d474eeece66cf1c63d0a5716dbab583d5a
codewitharun <arunk4it@gmail.com>
2023-06-25 06:55:14 +0530

    socket setup doned

--- src/index.js
+++ src/index.js
@@ -46,7 +46,10 @@ io.on('connection', (socket) => {
   socket.on('createRoom', (roomId) => {
     socket.join(roomId);
     console.log(`Socket ${socket.id} joined room ${roomId}`);
-    users[socket.id] = roomId;
+    // users[socket.id] = roomId;
+    
+     users["arun"] = roomId;
+     console.log(users["arun"],roomId)
     socket.emit('roomJoined', roomId);
   });
 
